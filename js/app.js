d059272dbe1bd1c7cc3a965b1da936ed11b49c13
NhShanto01 <mdnurhossain940@gmail.com>
2022-03-01 01:08:08 +0600

    complete search bar and button with js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-alert('you are in');
+// alert('you are in');
+const loadPhone = () => {
+    // console.log("btn click");
+
+    const input = document.getElementById('search-input');
+    // console.log("input click");
+    const error = document.getElementById('error-message');
+    // console.log("error click");
+    const inputValue = input.value;
+    // console.log(inputValue);isNaN(inputValue) ||
+
+    if (inputValue == "") {
+        // alert("please enter a number");
+        error.innerText = "please give a phone name";
+        input.value = '';
+        // main.innerHTML = '';
+    }
+}
+// fetch('https://openapi.programming-hero.com/api/phones?search')
